46783b632fde17bd8b29ebc041043ce86c715871
Pikachu46220 <143781846+Pikachu46220@users.noreply.github.com>
2023-09-27 10:50:24 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'test',
